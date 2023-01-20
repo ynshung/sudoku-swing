df5cee32dc3ef86824666a9d5fe692e57cec9a2b
Young Shung <me@ynshung.com>
2023-01-21 02:19:19 +0800

    add antialiasing to sudoku board

--- src/main/java/sudoku/SudokuPanel.java
+++ src/main/java/sudoku/SudokuPanel.java
@@ -1,11 +1,6 @@
 package sudoku;
 
-import java.awt.BasicStroke;
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.Font;
-import java.awt.Graphics;
-import java.awt.Graphics2D;
+import java.awt.*;
 import java.awt.event.*;
 import java.awt.font.FontRenderContext;
 
@@ -59,6 +54,10 @@ public class SudokuPanel extends JPanel {
 	protected void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		Graphics2D g2d = (Graphics2D) g;
+		RenderingHints rh = new RenderingHints(
+				RenderingHints.KEY_TEXT_ANTIALIASING,
+				RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+		g2d.setRenderingHints(rh);
 		g2d.setColor(new Color(1.0f,1.0f,1.0f));
 		
 		int slotWidth = this.getWidth()/puzzle.getNumColumns();
